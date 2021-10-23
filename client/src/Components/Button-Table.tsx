6024c94b88ded3b1f3eaddc1eb0deaf9255dc890
nickds54 <nicksalter98@gmail.com>
2021-10-23 15:40:43 -0500

    Creating Home Page and Buttons

--- client/src/Components/Button-Table.tsx
+++ client/src/Components/Button-Table.tsx
@@ -1,18 +1,56 @@
 import React from "react";
+import '../styles/Button-Table.css';
 
 const ButtonTable = () =>{
 
     const buttons: Array<number> = [];
 
-    for(var i = 0; i < 37; i++){
+    for(var i = 0; i < 12; i++){
         buttons.push(i);
     }
-    
+
+   
     return (
         <>
+        <div className="container-wrapper">
             {buttons.map((value, index) =>{
-                return <button>{value}</button>
+                var firsButton = (value + value) + (index + 1);
+                var secondButton = (value + value) + (index + 2);
+                var thirdButton = (value + value) + (index + 3);
+
+                    return(
+                        <>
+                        { <div className="button-wrapper">
+                            <button 
+                                className={`btn roulette-button ${firsButton % 2 === 0 ? 'white-button' : 'black-button'}`}
+                            >
+                                    {firsButton}
+                            </button>
+                            <button 
+                                className={`btn roulette-button ${secondButton % 2 === 0 ? 'white-button' : 'black-button'}`}
+                            >
+                                    {secondButton}
+                            </button>
+                            <button 
+                                className={`btn roulette-button ${thirdButton % 2 === 0 ? 'white-button' : 'black-button'}`}
+                            >
+                                    {thirdButton}
+                            </button>
+                        </div>}
+                        </>
+                    ) 
             })}
+            {/* <div className="button-wrapper">
+                <button>1</button>
+                <button>2</button>  
+                <button>3</button>  
+            </div>
+            <div className="button-wrapper">
+                <button>4</button>  
+                <button>5</button>  
+                <button>6</button>    
+            </div> */}
+        </div>
         </>
     )
 } 
--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import "../styles/HomePage.css";
+
 import * as Componentes from '../Components/index';
 
 const HomePage = () =>{
 
     return (
         <>
+        <div className="hompage-container">
             <Componentes.RouletteWheel/>
             <Componentes.ButtonTable/>
+        </div>
         </>
     )
 }
